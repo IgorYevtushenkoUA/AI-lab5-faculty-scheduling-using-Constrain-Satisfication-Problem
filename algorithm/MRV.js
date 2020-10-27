1956abb7b086ff56e0c2b075751b5a6d48fbdd58
Igor Yevtushenko <70605781+IgorYevtushenkoUA@users.noreply.github.com>
2020-10-28 00:14:59 +0200

    feat: print schedule

--- algorithm/MRV.js
+++ algorithm/MRV.js
@@ -2,9 +2,9 @@ import {data_teachers_map} from "../data/data_teacher.js";
 import {data_auditory_map} from "../data/data_auditory.js";
 import {data_map_disciplines, data_plurals, fillDisciplines, fillPluralsForDiscipline} from "../algorithm.js";
 import {Lesson} from "../plurals/Lesson.js";
+import {data_course_arr} from "../data/data_course.js";
 
 'use strict'
-
 
 /**
  * @param {Discipline} discipline
@@ -129,48 +129,48 @@ function solveSchedule(schedule, data_plurals, copyMap, originDisciplinesMap) {
     if (variablesList.length === 0) return false
 
     // for (let i = 0; i < variablesList.length; i++) {
-        let variant = variablesList[0]
-        data_plurals.set(variant, false)
+    let variant = variablesList[0]
+    data_plurals.set(variant, false)
 
 
-        // console.log("countWhenScheduleAdd :: " + countWhenScheduleAdd++)
-        schedule.push(new Lesson(dis, variant[0], variant[1], variant[2]))
+    // console.log("countWhenScheduleAdd :: " + countWhenScheduleAdd++)
+    schedule.push(new Lesson(dis, variant[0], variant[1], variant[2]))
 
-        queueOfDeletedOperation.push(dis)
-        copyMap.delete(dis)
+    queueOfDeletedOperation.push(dis)
+    copyMap.delete(dis)
 
-        //видаляємо [аудит-день-пара] із можливого списку кожної дисципліни тому що вона вже зайнята
+    //видаляємо [аудит-день-пара] із можливого списку кожної дисципліни тому що вона вже зайнята
 
-        copyMap = countConstraints(copyMap, data_plurals)
-        copyMap = sortByConstraints(copyMap)
+    copyMap = countConstraints(copyMap, data_plurals)
+    copyMap = sortByConstraints(copyMap)
 
-        if (originDisciplinesMap.length === schedule.length) {
-            return true
-        }
-        if (getElemByIndexInMap(copyMap, 0) === undefined) {
-            // console.log("cant solve do step back")
-            return false
-        } else {
-            solveSchedule(schedule, data_plurals, copyMap, originDisciplinesMap)
-        }
+    if (originDisciplinesMap.length === schedule.length) {
+        return true
+    }
+    if (getElemByIndexInMap(copyMap, 0) === undefined) {
+        // console.log("cant solve do step back")
+        return false
+    } else {
+        solveSchedule(schedule, data_plurals, copyMap, originDisciplinesMap)
+    }
     // }
     return true
 }
 
-function solveSchedulePRO(schedule, data_plurals, copyMap, originDisciplinesMap){
+function solveSchedulePRO(schedule, data_plurals, copyMap, originDisciplinesMap) {
     if (copyMap.size === 0) return true
     if (schedule.length === originDisciplinesMap.size) return true
 
     let delQueue = []
-    while(schedule.length !== originDisciplinesMap.size){
+    while (schedule.length !== originDisciplinesMap.size) {
         let item = getElemByIndexInMap(copyMap, 0)
         let dis = item[0]
         let disList = item[1]
 
-        for(let i = 0 ; i < disList.length; i++){
+        for (let i = 0; i < disList.length; i++) {
             let firstVar = disList[i]
             data_plurals.set(firstVar, false)
-            let newLesson = new Lesson(dis, disList[i][0], disList[i][1],disList[i][2])
+            let newLesson = new Lesson(dis, disList[i][0], disList[i][1], disList[i][2])
             delQueue.push(newLesson)
             schedule.push(newLesson)
             copyMap.delete(dis)
@@ -183,13 +183,13 @@ function solveSchedulePRO(schedule, data_plurals, copyMap, originDisciplinesMap)
                 data_plurals[firstVar] = true
                 copyMap.set(dis, disList)
                 delQueue.pop()
-            }else if (getElemByIndexInMap(copyMap, 0)[1].length === 0 && (i+1) === disList.length){
+            } else if (getElemByIndexInMap(copyMap, 0)[1].length === 0 && (i + 1) === disList.length) {
                 schedule.pop()
                 data_plurals[firstVar] = true
                 copyMap.set(dis, disList.slice(1))
                 delQueue.pop()
                 break; // to finish cycle
-            }else {
+            } else {
                 break // to finish cycle | another way is do i to big for instance := i+= disList.length
             }
         }
@@ -197,19 +197,88 @@ function solveSchedulePRO(schedule, data_plurals, copyMap, originDisciplinesMap)
     return true
 }
 
+function main() {
+    fillDisciplines()
+    fillPluralsForDiscipline()
 
-fillDisciplines()
-fillPluralsForDiscipline()
+    countConstraints(data_map_disciplines, data_plurals)
+    sortByConstraints(data_map_disciplines)
+    let firstIndexFromMap = getElemByIndexInMap(data_map_disciplines, 0)[0]
+    let plurals_with_most_constraints = getElemByIndexInMap(data_map_disciplines, 0)[1]
+    let schedule = []
+    let copyMap = new Map(data_map_disciplines)
+    solveSchedulePRO(schedule, data_plurals, copyMap, data_map_disciplines)
+    // console.log(solveSchedulePRO(schedule, data_plurals, copyMap, data_map_disciplines));
+    // console.log(schedule);
 
-countConstraints(data_map_disciplines, data_plurals)
-sortByConstraints(data_map_disciplines)
-let firstIndexFromMap = getElemByIndexInMap(data_map_disciplines, 0)[0]
-let plurals_with_most_constraints = getElemByIndexInMap(data_map_disciplines, 0)[1]
-let schedule = []
-let copyMap = new Map(data_map_disciplines)
-console.log(solveSchedulePRO(schedule, data_plurals, copyMap, data_map_disciplines));
-console.log(schedule);
+    printSchedule(schedule)
+}
 
-// for (let i = 0; i < plurals_with_most_constraints.length; i++) {
-// }
+function getDay(num) {
+    switch (num) {
+        case 0 :
+            return "Monday"
+        case 1 :
+            return "Tuesday"
+        case 2 :
+            return "Wednesday"
+        case 3 :
+            return "Thursday"
+        case 4 :
+            return "Friday"
+        case 5 :
+            return "Saturday"
+    }
+}
+
+function getPair(num) {
+    switch (num) {
+        case 1 :
+            return "Para 1"
+        case 2 :
+            return "Para 2"
+        case 3 :
+            return "Para 3"
+        case 4 :
+            return "Para 4"
+        case 5 :
+            return "Para 5"
+        case 6 :
+            return "Para 6"
+        case 7 :
+            return "Para 7"
+    }
+}
+
+function printSchedule(schedule){
+    for (let course = 0; course < data_course_arr.length; course++) {
+        console.log( "\n" + data_course_arr[course].getCourseName)
+        for (let day = 0; day < 6; day++) {
+            console.log(getDay(day))
+            for (let pair = 0; pair < 7; pair++) {
+
+                let auditory = "~~~"
+                let discipline = "~~~"
+                for (let lesson = 0; lesson < schedule.length ; lesson++){
+                    let l1 = schedule[lesson].getLessonDiscipline
+                    let l2 = schedule[lesson].getLessonDiscipline.getDisciplineGroup
+                    let l3 = schedule[lesson].getLessonDiscipline.getDisciplineGroup.getStudentGroupName
+
+                    if (schedule[lesson].getLessonDiscipline.getDisciplineGroup.getStudentGroupCourseName === data_course_arr[course].getCourseName) {
+                        if (schedule[lesson].getLessonDay === day){
+                            if (schedule[lesson].getLessonPair === pair){
+                                auditory = schedule[lesson].getLessonAuditory
+                                discipline = schedule[lesson].getLessonDiscipline.getDisciplineGroup
+                                console.log(getPair(pair) + " | Auditory :: " + auditory + "\t| Discipline :: " + discipline)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+main()
+
 
--- plurals/Discipline.js
+++ plurals/Discipline.js
@@ -13,7 +13,7 @@ export class Discipline {
         this._disciplineGroup       = groupName
     }
 
-    get getDisciplineDiscipline()       {return this._disciplineName}
+    get getDisciplineName()       {return this._disciplineName}
     get getDisciplineType()             {return this._disciplineType}
     get getDisciplineGroup()            {return this._disciplineGroup}
     get getDisciplineTeacher()          {return this._disciplineTeacher}
